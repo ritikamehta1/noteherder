adce638bde30dd3cfedd805f8fcfeed2a3d7cc58
Ritika Mehta <rimehta@iu.edu>
2018-05-22 11:59:08 -0400

    begin styling

--- noteherder/src/Sidebar.js
+++ noteherder/src/Sidebar.js
@@ -5,30 +5,60 @@ import newIcon from './new.png'
 import newHover from './new-hover.png'
 
 const Sidebar = () => {
-    return(
-        <div className="Sidebar">
-            <div className ="logo">
-                <img src={quill} alt="Noteherder" />
-            </div>
+  return (
+    <div
+      className="Sidebar"
+      style={styles.sidebar}
+    >
+      <div
+        className="logo"
+        style={styles.logo}
+      >
+        <img
+          src={quill}
+          alt="Noteherder"
+          style={styles.logoImg}
+        />
+      </div>
 
-            <a> href="/notes">
-                <img
-                    src={newHover}
-                    alt='New note'
-                />
-                <img
-                    src={newIcon}
-                    alt='nNew note'
-                />
-            </a>
+      <a href="/notes">
+        <img
+          src={newHover}
+          alt="New note"
+        />
+        <img
+          src={newIcon}
+          alt="New note"
+        />
+      </a>
 
-            <div className="SignOut">
-                <button>
-                    <i classname="fa fa-sign-out"></i>
-                </button> 
-            </div>
-        </div>
-    )
+      <div className="SignOut">
+        <button>
+          <i className="fa fa-sign-out"></i>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+const styles = {
+  sidebar: {
+    width: '6rem',
+    backgroundColor: '#f3f3f3',
+    padding: '0.5rem 0',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  logo: {
+    fontFamily: '"Fauna One"',
+    color: '#666',
+    fontSize: '3rem',
+  },
+  logoImg: {
+    width: '3rem',
+    paddingLeft: '0.4rem',
+  }
 }
 
 export default Sidebar
