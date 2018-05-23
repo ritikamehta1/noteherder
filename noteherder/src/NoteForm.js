b976d39ae6a0fab37833514d72ad6f6402ae7fe0
Ritika Mehta <rimehta@iu.edu>
2018-05-23 10:55:36 -0400

    set current note when clicking note

--- noteherder/src/NoteForm.js
+++ noteherder/src/NoteForm.js
@@ -1,65 +1,28 @@
 import React from 'react'
 
-const NoteForm = () => {
-    return (
-        <div className="NoteForm">
-        NoteForm
-        </div>
-    )
-}
+import './NoteForm.css'
 
-const styles = {
-    NoteForm: {
-        msFlexPositive: '1',
-        flexGrow: '1',
-        padding: '0 3rem',
-      },
-    formActions: {
-        paddingTop: '1rem',
-        marginLeft: '-2rem',
-        display: '-ms-flexbox',
-        display: 'flex',
-        msFlexLinePack: 'center',
-        alignContent: 'center',
-      },
-      button: {
-        border: 'none',
-        background: 'none',
-        padding: '0',
-      },
-      buttonTypeSubmit: {
-        backgroundColor: '#008bf8',
-        borderRadius: '4px',
-        color: '#fff',
-        fontSize: '1.4rem',
-        padding: '1rem',
-      },
-      buttonifa: {
-        color: '#999',
-        fontSize: '2rem',
-        margin: '0',
-      },
-      form: {
-        margin: '0 auto',
-        maxWidth: '80rem',
-      },
-      inputNameTitle: {
-        border: 'none',
-        fontSize: '200%',
-        fontFamily: '"Fauna One"',
-        color: '#008bf8',
-        fontWeight: '400',
-        width: '100%',
-        outline: 'none',
-      },
-      textarea: {
-        borderColor: '#eee',
-        width: '100%',
-        height: 'calc(100vh - 140px)',
-        fontSize: '1.3em',
-      }
-    }
-      
-    
+const NoteForm = () => {
+  return (
+    <div className="NoteForm">
+      <div className="form-actions">
+        <button type="button">
+          <i className="far fa-trash-alt"></i>
+        </button>
+      </div>
+      <form>
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title your note"
+          />
+        </p>
+
+        <textarea name="body"></textarea>
+      </form>
+    </div>
+  )
+}
 
 export default NoteForm
