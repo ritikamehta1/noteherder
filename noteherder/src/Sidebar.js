8a0480e309ee672ba37d126d4649c977500c4e20
Ritika Mehta <rimehta@iu.edu>
2018-05-23 09:39:08 -0400

    aphrodite styling

--- noteherder/src/Sidebar.js
+++ noteherder/src/Sidebar.js
@@ -4,41 +4,71 @@ import quill from './quill.svg'
 import newIcon from './new.png'
 import newHover from './new-hover.png'
 
-const Sidebar = () => {
-  return (
-    <div
-      className="Sidebar"
-      style={styles.sidebar}
-    >
-      <div
-        className="logo"
-        style={styles.logo}
+class Sidebar extends React.Component {
+  state = {
+    newIconHovered: false,
+  }
+
+  handleMouseEnter() {
+    this.setState({ newIconHovered: true })
+  }
+
+  handleMouseLeave() {
+    this.setState({ newIconHovered: false })
+  }
+
+  render() {
+    return (
+      <nav
+        className="Sidebar"
+        style={styles.sidebar}
       >
-        <img
-          src={quill}
-          alt="Noteherder"
-          style={styles.logoImg}
-        />
-      </div>
-
-      <a href="/notes">
-        <img
-          src={newHover}
-          alt="New note"
-        />
-        <img
-          src={newIcon}
-          alt="New note"
-        />
-      </a>
-
-      <div className="SignOut">
-        <button>
-          <i className="fa fa-sign-out"></i>
-        </button>
-      </div>
-    </div>
-  )
+        <div
+          className="logo"
+          style={styles.logo}
+        >
+          <img
+            src={quill}
+            alt="Noteherder"
+            style={styles.logoImg}
+          />
+        </div>
+        <a
+          className="new-note"
+          href="/notes"
+          style={styles.newNote}
+          onMouseEnter={() => this.handleMouseEnter()}
+          onMouseLeave={() => this.handleMouseLeave()}
+        >
+          <img
+            src={newHover}
+            alt="New note"
+            style={styles.newNoteImg}
+          />
+          <img
+            className="outline"
+            src={newIcon}
+            alt="New note"
+            style={{
+              ...styles.newNoteImg,
+              opacity: this.state.newIconHovered ? 0 : 1
+            }}
+          />
+        </a>
+        <div
+          className="SignOut"
+          style={styles.signOut}
+        >
+          <button style={styles.button}>
+            <i
+              className="fa fa-sign-out"
+              style={styles.buttonIcon}
+            ></i>
+          </button>
+        </div>
+      </nav>
+    )
+  }
 }
 
 const styles = {
@@ -59,37 +89,31 @@ const styles = {
     width: '3rem',
     paddingLeft: '0.4rem',
   },
-  aNewNote: {
+  newNote: {
     marginTop: '2rem',
     position: 'relative',
-    width: '40px',
+    width: '4rem',
   },
-  button: {
-    backgroundColor: 'transparent',
-    border: '0',
-    color: '#008BF8',
-    cursor: 'pointer',
-  },
-  aImg: {
+  newNoteImg: {
     position: 'absolute',
-    left: '0rem',
+    left: '0',
     width: '100%',
     transition: 'opacity 0.25s ease-in-out',
-  },
-  hoverImgOutline: {
-    opacity: '0rem',
   },
   signOut: {
     position: 'absolute',
     bottom: '1rem',
   },
-  signOutButton: {
+  button: {
+    backgroundColor: 'transparent',
+    border: '0',
+    color: '#008bf8',
+    cursor: 'pointer',
     outline: 'none',
   },
-  signOutButtonifa: {
+  buttonIcon: {
     fontSize: '2rem',
-  }
-  
+  },
 }
 
 export default Sidebar
